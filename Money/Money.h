4f208ac2f89e8afc79dc66ddd6449dd05c5083ca
Krystian Kopeć <krystian.kopec@mobile-5.com>
2019-04-17 22:09:23 +0200

    Initial Commit

---
+++ Money/Money.h
@@ -0,0 +1,19 @@
+//
+//  Money.h
+//  Money
+//
+//  Created by Krystian Kopeć on 17/04/2019.
+//  Copyright © 2019 Krystian Kopeć. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Money.
+FOUNDATION_EXPORT double MoneyVersionNumber;
+
+//! Project version string for Money.
+FOUNDATION_EXPORT const unsigned char MoneyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Money/PublicHeader.h>
+
+
